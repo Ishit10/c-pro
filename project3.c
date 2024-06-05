beca1b46847d90097472c126466cf1a83df80489
ishit10 <kapuparaishit18@gmail.com>
2024-06-05 17:59:51 +0530

    first commit

---
+++ project3.c
@@ -0,0 +1,44 @@
+#include  <stdio.h>
+int main(){
+    // Ans1
+
+    // char alf='a';
+    // do{
+    //     printf("%c\n",alf);
+    //     alf+=4;
+    // }while(alf<='z');
+    // printf(" \n ");
+
+    // Ans  2
+
+    // int num, count=0;
+    // printf("Enter your num:");
+    // scanf("%d",&num);
+    //  int tem=num;
+    //  if(tem==0){
+    //     count=1;
+    //  }else{
+    //     for(;tem!=0; tem/=10){
+    //         count++;
+    //     }
+    //  }
+    //  printf("the total num of digit %d is : %d\n",num,count);
+
+    // Ans 3
+
+    // int num,fristdigit,lastdigit,t;
+    // printf("Enter your num:");
+    // scanf("%d",&num);
+
+    // lastdigit=num%10;
+    // t=num;
+
+    // for(;t>=10; t/=10);
+    // fristdigit=t;
+
+    // int sum=fristdigit+lastdigit;
+
+    // printf("The sum of %d is : %d\n",num,sum);
+
+    return 0;
+}
---
+++ project4.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+int main(){
+    // ans 1
+
+//    int r;
+//    int start=41;
+//    printf("Enter your value: ");
+//    scanf("%d",&r);
+//    for(int i=0; i<=r; i++){
+//     for(int j=0; j<=i; j++){
+//         printf("%d ",start+j);
+//     }
+//     printf("\n");
+//    }
+  
+
+//   Ans 2
+
+// int start=11;
+// int r;
+// int currentnumber=start;
+
+// printf("enter your value:");
+// scanf("%d",&r);
+
+// for(int i=0; i<=r; i++){
+//     for(int j=0; j<=i; j++){
+//         printf("%d ",currentnumber);
+//         currentnumber++;
+//     }
+//     printf("\n");
+// }
+
+
+// Ans 3
+
+
+//   int r=5;
+//   for(int i=r; i>=1; i--){
+//     for(int j=r; i<i; j++){
+//         printf(" ");
+//     }
+//     for(int a=i; a<=r; a++){
+//     printf("%d",a);
+//   }
+//     printf("\n");
+//   }
+  
+
+//   Ans 4
+
+
+   
+
+    return 0;
+}
