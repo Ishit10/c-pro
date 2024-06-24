a116eee50ca66134dadafad4123995411f23f681
ishit10 <kapuparaishit18@gmail.com>
2024-06-24 17:37:31 +0530

    done

--- project4.c
+++ project4.c
@@ -16,41 +16,97 @@ int main(){
 
 //   Ans 2
 
-// int start=11;
-// int r;
-// int currentnumber=start;
+// int n=4;
+// int k=11;
 
-// printf("enter your value:");
-// scanf("%d",&r);
+// for(int i=1; i<=n; i++){
+//     for(int j=1; j<=i; j++){
+//         printf("%d ",k++);
+//     }
+//     printf("\n");
+// }
 
-// for(int i=0; i<=r; i++){
-//     for(int j=0; j<=i; j++){
-//         printf("%d ",currentnumber);
-//         currentnumber++;
+// Ans 3
+
+
+// int n=5;
+// for(int i=n; i>=1; i--){
+//     for(int j=1; j<i; j++){
+//         printf("  ");
+//     }
+//     for(int j=i; j<=n; j++){
+//         printf("%d ",j);
+//     }
+    
+//     printf("\n");
+// }
+
+
+// Ans 4
+
+
+// int n=5;
+// for(int i=n; i>=1; i--){
+//     for(int j=1; j<=n-i; j++){
+//         printf("  ");
+//     }
+//     for(int j=1; j<=i; j++){
+//         printf("%d ",j%2);
 //     }
 //     printf("\n");
 // }
 
 
-// Ans 3
+// Ans 5
+
+// int n=5;
+// for(int i=n;i>=1; i--){
+//     for(int j=1; j<i; j++){
+//         printf("  ");
+//     }
+//     for(int j=i; j<=n; j++){
+//         printf("%d ",j);
+//     }
+//     for(int k=n-1; k>=i; k--){
+//         printf("%d ",k);
+//     }
+//     printf("\n");
+// }
 
 
-//   int r=5;
-//   for(int i=r; i>=1; i--){
-//     for(int j=r; i<i; j++){
+// Ans 6
+
+
+// int n=5;
+// for(int i=1; i<=n; i++){
+//     for(int j=1; j<=i; j++){
+//         printf("%d",j);
+//     }
+//     for(int k=1; k<=n-i; k++){
 //         printf(" ");
 //     }
-//     for(int a=i; a<=r; a++){
-//     printf("%d",a);
-//   }
+    
+//     for(int k=1; k<=n-i; k++){
+//         printf(" ");
+//     }
+    
+//     for(int j=i; j>=1; j--){
+//         printf("%d",j);
+//     }
+    
+    
 //     printf("\n");
-//   }
-  
-
-//   Ans 4
+// }
 
 
-   
+// Ans 7
+// printf("* * * * *\n");
+// printf("*       *\n");
+// printf("*       *\n");
+// printf("* * * * *\n");
+// printf("*\n");
+// printf("*\n");
+
 
     return 0;
 }
